daa21b6e97a4c73373b15577f47b3f42531e6e59
avinash0408 <avinash.08jan2001@gmail.com>
2024-09-16 22:33:28 +0530

    .

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -3,6 +3,11 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.route('/').get((req,res)=>{
+    res.json({
+        message : "Hi there!!" 
+    })
+})
 router.route('/signup').post(authController.signup);
 router.route('/signin').post(authController.login);
 router.route('/signout').get(authController.logout);
