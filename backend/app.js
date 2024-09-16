ffdcf0f30aa794954cafbb64790b629b326712c2
avinash0408 <avinash.08jan2001@gmail.com>
2024-09-17 00:36:46 +0530

    cors-handler

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin : 'vi-todo-app.vercel.app',
+    origin : 'https://vi-todo-app.vercel.app',
     methods: 'GET,POST,PUT,DELETE,PATCH'
 }));
 app.use(express.json());
